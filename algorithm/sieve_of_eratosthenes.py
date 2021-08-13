5097fb00cc999986077f36977a09d690e7d3d5b7
WHITE-GRINT\Kazuki <kzktcy0610@gmail.com>
2021-08-14 02:28:06 +0900

    エラストテネスの篩、多分アルゴリズムとしては間違ってはいないが、掛け算の個数など効率悪い箇所が多い。。removeは遅いのか。

--- algorithm/sieve_of_eratosthenes.py
+++ algorithm/sieve_of_eratosthenes.py
@@ -24,21 +24,32 @@ def erastosthenes(number):
         return
 
     # 平方根にして整数化する、これが検索のMAX
-    squrt_limit = int(math.sqrt(number))
+    max_limit = int(math.sqrt(number))
+    print('Max: ' + str(max_limit))
 
-    # 2からが素数なので2と3以上の奇数のリストを作る(2より大きい偶数は素数でないので無視)、
-    # このリストから割れる数字を除いたものが素数
-    list = [2]
-    list += [i + 1 for i in range(2, number, 2)]
-
-    for i in list:
-        print(i)
-        
+    # 3以上の奇数のリストを作る(2より大きい偶数は素数でないので無視)
+    odd_numbers = [i + 1 for i in range(2, number, 2)]
+    print(odd_numbers)
     
+    # max_limitまでの奇数のリスト
+    max_limit_odd_numbers = [i + 1 for i in range(2, max_limit, 2)]
+    print(max_limit_odd_numbers)
+    
+    # 返却用のリスト(2は素数なので入れておく)
+    prime_list = [2]
 
-
+    # 限界の数値を倍数していく、numbersに存在したら削除する
+    for odd_num in max_limit_odd_numbers:
         
+        for multiple_num in range (2, number // max_limit_odd_numbers[0]):
+            del_num = odd_num * multiple_num
+            if del_num in odd_numbers:
+                odd_numbers.remove(del_num)
+            
+    prime_list = prime_list + odd_numbers
 
+    print(prime_list)
+    print(len(prime_list))
 
 if __name__ == "__main__":
     main()
