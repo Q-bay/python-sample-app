e7dbeae2a2252d0bb96001c96ce6547837a73855
WHITE-GRINT\Kazuki <kzktcy0610@gmail.com>
2021-08-17 23:23:08 +0900

    一つのリストで倍数を0に変えるやり方(ネットの人に教わった方法)、速い

--- algorithm/sieve_of_eratosthenes.py
+++ algorithm/sieve_of_eratosthenes.py
@@ -12,7 +12,7 @@ def main():
         exit()
 
     if args[1].isdigit() == True:
-        erastosthenes(int(args[1]))
+        eratosthenes(int(args[1]))
     else:
         print('Please specify a numerical value for the argument.')
 
@@ -20,52 +20,14 @@ def main():
     print ("elapsed_time:{0}".format(elapsed_time) + "[sec]")
     print('end!')
 
-def erastosthenes(number):
-    print('check number: ' + str(number))
-    
-    if number == 1:
-        print('prime number is nothing.')
-        return
-
-    # 平方根にして整数化する、これが検索のMAX
-    max_limit = int(math.sqrt(number))
-    #print('Max: ' + str(max_limit))
-
-    # 3以上の奇数のリストを作る(2より大きい偶数は素数でないので無視)
-    odd_numbers = [i for i in range(3, number, 2)]
-    #print('odd_numbers: ' + str(odd_numbers))
-    
-    # max_limitまでの奇数のリスト
-    max_limit_odd_numbers = [i for i in range(3, max_limit, 2)]
-    #print('max_limit_odd_numbers: ' + str(max_limit_odd_numbers))
-    
-    # max_limitから素数だけにする
-    max_limit_prime_numbers = make_prime_list(max_limit_odd_numbers, max_limit_odd_numbers)
-    #print('max_limit_prime_numbers: ' + str(max_limit_prime_numbers))
-    
-    # 返却用のリスト(2は素数なので入れておく)
-    ret_prime_list = [2] + make_prime_list(odd_numbers, max_limit_prime_numbers)
-    #print(ret_prime_list)
-    print('prime number quantity is :' + str(len(ret_prime_list)))
-
-def make_prime_list(num_list1, num_list2):
-    prime_list = []
-    for num1 in num_list1:
-        add_flag = True
-        for num2 in num_list2:
-            
-            if num1 <= num2:
-                add_flag = True
-                break
-
-            if num1 % num2 == 0:
-                add_flag = False
-                break
-            
-        if add_flag == True:
-            prime_list.append(num1)
-
-    return prime_list
+def eratosthenes(n):
+    print("check number: {0}".format(n))
+    primes = list(range(0, n + 1))
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if primes[i] > 0:
+            for j in range(2 * i, n + 1, i):
+                primes[j] = 0
+    print("prime number quantity is :{0}".format(sum(x > 0 for x in primes[2:])))
 
 if __name__ == "__main__":
     main()
