54bae4cba7e02e27463d1f95a66e0780bda803ad
WHITE-GRINT\Kazuki <kzktcy0610@gmail.com>
2022-01-16 23:09:38 +0900

    二分探索

--- algorithm/binary_search.py
+++ algorithm/binary_search.py
@@ -13,14 +13,29 @@ def main():
         exit()
 
     if args[1].isdigit() == True:
-        binary_search(target, int(args[1]))
+        print(binary_search(target, int(args[1])))
     else:
         print('Please specify a numerical value for the argument.')
-
     print('end!' )
 
 def binary_search(taraget, n):
     left = 0
-    right = len(target) -1
-    
+    right = len(target) - 1
 
+    while left <= right:
+        print('left: {0}'.format(left))
+        print('right: {0}'.format(right))
+    
+        mid = (left + right) // 2
+        print('mid: {0}'.format(mid))
+
+        if(target[mid] == n):
+            return mid
+        elif target[mid] < n:
+            left = mid + 1
+        else:
+            right = mid -1
+    return -1
+
+if __name__ == "__main__":
+    main()
